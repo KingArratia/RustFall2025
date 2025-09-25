f491f4046cb5be04bff65551c9218d0c1558f602
KingArratia <barratia19@gmail.com>
2025-09-25 01:50:01 +0000

    Rust Borrow Practice in-Class

---
+++ class_work/target/debug/class_work.d
@@ -0,0 +1,1 @@
+/workspaces/RustFall2025/class_work/target/debug/class_work: /workspaces/RustFall2025/class_work/src/main.rs
---
+++ class_work/target/debug/deps/class_work-dcbfaeac266e559e.d
@@ -0,0 +1,5 @@
+/workspaces/RustFall2025/class_work/target/debug/deps/class_work-dcbfaeac266e559e.d: src/main.rs
+
+/workspaces/RustFall2025/class_work/target/debug/deps/class_work-dcbfaeac266e559e: src/main.rs
+
+src/main.rs:
